6b8baa1c027b4f3a4d94347214f8a240023aeb41
andela-gike <grace.ike@andela.com>
2020-03-25 14:17:26 +0100

    modify test to pass accordingly

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('twitter-scrapping app is running!');
+    expect(compiled.querySelector('.toolbar span').textContent).toContain('Angular Twitter');
   });
 });
